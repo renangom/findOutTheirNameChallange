8d6fa1c449aa0886aa4e204cf2c25368e9aacbc3
Renan <renan23eduardo@gmail.com>
2022-12-14 16:00:49 -0300

    Create numberOfSyllabes function

--- src/functions/numberOfSyllabes.ts
+++ src/functions/numberOfSyllabes.ts
@@ -1,14 +1,15 @@
 import countVowels from "./countVowels";
 
 //usually the number of vowels is the number of 
+//this function was made for brazilian words
 export default function numberOfSyllabes(text:string):number{
     const vowels: Array<string> = ["a", "e", "i", "o", "u"]
-    let vowelsCount:number = 0;
+    let syllabeCount:number = 0;
     let i;
     for(i = 0; i < text.length; i++){
         if(vowels.indexOf(text[i].toLowerCase()) != -1){
-            vowelsCount++
+            syllabeCount++
         }
     }
-    return vowelsCount;
+    return syllabeCount;
 }
