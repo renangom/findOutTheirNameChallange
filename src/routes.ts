34d34da6bcbef99c041e00b391ac5e0c23fa6a1c
Renan <renan23eduardo@gmail.com>
2022-12-18 23:14:25 -0300

    Change de route

--- src/routes.ts
+++ src/routes.ts
@@ -1,51 +1,13 @@
 import { Request, Response, Router } from "express";
-import multer from "multer";
-import { Readable } from "stream";
 import readline from 'readline';
 import findName from "./functions/findNameFunction";
 import fs from 'fs'
 import checkingBody from "./middleWares/checkingBody";
 
 export const routes = Router()
-const multerConfig = multer();
 
 let names:Array<string> = [];
-//multer works as a middleware
-routes.post("/names", multerConfig.single('file') , async (req:Request, res:Response) => {
-
-    const {includes, beginsWith, vowels, syllabe} = req.body;
-    console.log(includes, beginsWith, vowels, syllabe)
-    const buffer = req.file?.buffer
-
-    //here we got the file
-    const readableFile = new Readable();
-    readableFile.push(buffer);
-    readableFile.push(null);
-
-    //now I want to read this file line by line
-    const namesLine = readline.createInterface({
-        input: readableFile
-    })
-
-
-    for await (let line of namesLine) {
-        const nameSplit = line.split(`"`)
-        names.push(nameSplit[1].toString())
-    }
-    let namesFounded: Array<string> = []
-
-    namesFounded = findName(names, includes, beginsWith, Number(vowels), Number(syllabe))
-
-    if(names.length === 0){
-        res.json("There are no names with this parameters")
-    }else if(names.length === 1){
-        res.json("Here it is my friend " + namesFounded )
-    }else {
-        res.json("The possible names are " + namesFounded)
-    }
-})
-
-routes.post("/names2",checkingBody, (req:Request, res:Response) => {
+routes.post("/names",checkingBody, (req:Request, res:Response) => {
     //variable
     const includes:string =  req.body.includes;
     const beginsWith:string = req.body.beginsWith;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,5 @@
 import express from 'express';
 import  names, { routes } from './routes';
-import fs, { read } from 'fs'
-import readline from 'readline'
-import { Readable } from 'stream';
-import findName from './functions/findNameFunction';
 
 const app = express();
 app.use(express.json());
